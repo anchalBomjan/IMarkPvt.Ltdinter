04afa28959042451a98fae3e85c463232f615006
anchalBomjan <anchal.percoidit@gmail.com>
2025-05-18 17:52:40 +0545

    completing all the method of  application layers

--- cleanarchitectureforasp.netcorewebapi/StudentManagement/Application/StudentService.cs
+++ cleanarchitectureforasp.netcorewebapi/StudentManagement/Application/StudentService.cs
@@ -11,5 +11,70 @@ namespace StudentManagement.Application
         }
 
         
+        public async Task<IEnumerable<StudentDTO>> GetAllStudentsAsync()
+        {
+            var students=await _studentRepository.GetAllStudentsAsync();
+            return students.Select(s => new StudentDTO 
+            { 
+             Id = s.Id,
+             Name = s.Name,
+             Email = s.Email,
+             Age = s.Age,
+            
+            
+            
+            }).ToList();
+
+          
+
+        }
+
+
+
+        public async Task AddStudentAsync(StudentDTO studentDto)
+        {
+            var student = new Student
+            {
+                Name = studentDto.Name,
+                Email = studentDto.Email,
+                Age = studentDto.Age,
+
+            };
+
+            student.ValidateAge();
+            await _studentRepository.AddStudentAsync(student);
+        }
+
+
+        public async Task UpdateStudentAsync(int id, StudentDTO studenmtDto)
+        {
+            var student = await _studentRepository.GetStudentByIdAsync(id);
+            if(student == null)
+            
+                throw new Exception("Student not found");
+
+            
+
+
+            student.Name = studenmtDto.Name;
+            student.Email = studenmtDto.Email;
+            student.Age= studenmtDto.Age;
+            student.ValidateAge() ;
+            await _studentRepository.UpdateStudentAsync(student);
+
+
+
+        }
+
+
+        public async Task DeleteStudentAsync(int id)
+        {
+            await _studentRepository.DeleteStudentAsync(id);
+
+
+        }
+
+
+
     }
 }
