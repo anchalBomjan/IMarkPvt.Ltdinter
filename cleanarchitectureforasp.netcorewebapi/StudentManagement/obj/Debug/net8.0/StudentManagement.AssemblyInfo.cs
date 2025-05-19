4c7d872c203e8ab4cd6dd16f8a49503c99859b95
anchalBomjan <anchal.percoidit@gmail.com>
2025-05-19 13:47:33 +0545

    some changes

--- cleanarchitectureforasp.netcorewebapi/StudentManagement/obj/Debug/net8.0/StudentManagement.AssemblyInfo.cs
+++ cleanarchitectureforasp.netcorewebapi/StudentManagement/obj/Debug/net8.0/StudentManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04afa28959042451a98fae3e85c463232f615006")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44fc5819abe1dc3f2c96e859db48b8a52f51dac7")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
