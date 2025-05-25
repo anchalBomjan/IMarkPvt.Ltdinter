0c8d915bef6605fb5d58392e8a2ef58a0c0addce
anchalBomjan <anchal.percoidit@gmail.com>
2025-05-26 00:34:35 +0545

    injecting applicationDbContext in presentation layer

--- cleanarchitectureFortwotableCrudOperation/cleanarchitectureFortwotableCrudOperation/Program.cs
+++ cleanarchitectureFortwotableCrudOperation/cleanarchitectureFortwotableCrudOperation/Program.cs
@@ -1,4 +1,6 @@
 
+using Infracturcture.Presistance;
+
 namespace cleanarchitectureFortwotableCrudOperation
 {
     public class Program
@@ -10,6 +12,9 @@ namespace cleanarchitectureFortwotableCrudOperation
             // Add services to the container.
 
             builder.Services.AddControllers();
+
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
