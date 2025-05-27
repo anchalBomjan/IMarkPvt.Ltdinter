75c5c78d16594615614948ba64b773c5e4ad63e2
anchalBomjan <anchal.percoidit@gmail.com>
2025-05-27 17:21:43 +0545

    some chnages

--- cqrMediatorRcleanarchiitecture/cqrsMediator.Application/Common/Exceptions/ValidationException.cs
+++ cqrMediatorRcleanarchiitecture/cqrsMediator.Application/Common/Exceptions/ValidationException.cs
@@ -1,35 +1,29 @@
 ﻿using Microsoft.IdentityModel.Tokens;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace cqrsMediator.Application.Common.Exceptions
 {// Application/Common/Exceptions/ValidationException.cs
 
 
-    //public class ValidationException : Exception
-    //{
-    //    public IDictionary<string, string[]> Errors { get; }
+    public class ValidationException : Exception
+    {
+        public IDictionary<string, string[]> Errors { get; }
 
-    //    public ValidationException()
-    //        : base("Validation failures occurred.")
-    //    {
-    //        Errors = new Dictionary<string, string[]>();
-    //    }
+        public ValidationException()
+            : base("Validation failures occurred.")
+        {
+            Errors = new Dictionary<string, string[]>();
+        }
 
-    //    public ValidationException(IEnumerable<ValidationFailure> failures)
-    //        : this()
-    //    {
-    //        Errors = failures
-    //            .GroupBy(e => e.PropertyName, e => e.ErrorMessage)
-    //            .ToDictionary(failureGroup => failureGroup.Key, failureGroup => failureGroup.ToArray());
-    //    }
-    //}
+        public ValidationException(IEnumerable<ValidationFailure> failures)
+            : this()
+        {
+            Errors = failures
+                .GroupBy(e => e.Property, e => e.Error)
+                .ToDictionary(failureGroup => failureGroup.Key, failureGroup => failureGroup.ToArray());
+        }
+    }
 
     /// you have to install FluentValidation packages
-    
+
 
 
 }
