96bc612d6d4442c9f7ee1eb7d9f6a3c657e22e45
anchalBomjan <anchal.percoidit@gmail.com>
2025-06-01 12:00:00 +0545

    adding Ordering  web API

---
+++ Odering.API/Odering.Core/Repositories/Command/Base/ICommandRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odering.Core.Repositories.Command.Base
+{
+    public  interface ICommandRepository<T> where T : class
+    {
+        Task<T> AddAsync(T entitiy);
+        Task UpdateAsync(T entitiy);
+        Task DeleteAsync(T entitiy);
+
+    }
+}
