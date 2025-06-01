1662d3b46714f448654954e16fb8a97736a4c34b
anchalBomjan <anchal.percoidit@gmail.com>
2025-06-01 23:48:23 +0545

    ading DTOs in Application layers

--- Odering.API/Odering.Application/obj/Debug/net8.0/Odering.Application.AssemblyInfo.cs
+++ Odering.API/Odering.Application/obj/Debug/net8.0/Odering.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Odering.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bdfa7755ece38ec438e3a4d61f29be992dda56d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f130fa4352d693d24cb9698f480dba0901e0cf15")]
 [assembly: System.Reflection.AssemblyProductAttribute("Odering.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Odering.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
