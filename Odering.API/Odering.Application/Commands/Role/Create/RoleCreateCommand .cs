737d7f0c92a052c63e67a6c3a8731bdc3743c09e
anchalBomjan <anchal.percoidit@gmail.com>
2025-06-02 01:11:08 +0545

    adding Commands and Queries in Application Layers

---
+++ Odering.API/Odering.Application/Commands/Role/Create/RoleCreateCommand .cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using Odering.Application.Common.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odering.Application.Commands.Role.Create
+{
+    public class RoleCreateCommand : IRequest<int>
+    {
+        public string RoleName { get; set; }
+    }
+
+    public class RoleCreateCommandHandler : IRequestHandler<RoleCreateCommand, int>
+    {
+        private readonly IIdentityService _identityService;
+
+        public RoleCreateCommandHandler(IIdentityService identityService)
+        {
+            _identityService = identityService;
+        }
+        public async Task<int> Handle(RoleCreateCommand request, CancellationToken cancellationToken)
+        {
+            var result = await _identityService.CreateRoleAsync(request.RoleName);
+            return result ? 1 : 0;
+        }
+    }
+
+}
---
+++ Odering.API/Odering.Application/Queries/Customers/GetAllCustomerQuery .cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using Odering.Core.Entities;
+using Odering.Core.Repositories.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odering.Application.Queries.Customers
+{
+    // Customer query with List<Customer> response
+    public record GetAllCustomerQuery : IRequest<List<Customer>>
+    {
+
+    }
+
+    public class GetAllCustomerHandler : IRequestHandler<GetAllCustomerQuery, List<Customer>>
+    {
+        private readonly ICustomerQueryRepository _customerQueryRepository;
+
+        public GetAllCustomerHandler(ICustomerQueryRepository customerQueryRepository)
+        {
+            _customerQueryRepository = customerQueryRepository;
+        }
+        public async Task<List<Customer>> Handle(GetAllCustomerQuery request, CancellationToken cancellationToken)
+        {
+            return (List<Customer>)await _customerQueryRepository.GetAllAsync();
+        }
+    }
+
+}
