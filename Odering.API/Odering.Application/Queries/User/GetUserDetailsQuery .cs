28070f8ff1710091e50bdc7bb0ed0296bf838f73
anchalBomjan <anchal.percoidit@gmail.com>
2025-06-02 22:55:30 +0545

    adding other queries of User

---
+++ Odering.API/Odering.Application/Queries/User/GetUserDetailsQuery .cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using Odering.Application.Common.Interface;
+using Odering.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odering.Application.Queries.User
+{
+
+    public class GetUserDetailsQuery : IRequest<UserDetailsResponseDTO>
+    {
+        public string UserId { get; set; }
+    }
+
+    public class GetUserDetailsQueryHandler : IRequestHandler<GetUserDetailsQuery, UserDetailsResponseDTO>
+    {
+        private readonly IIdentityService _identityService;
+
+        public GetUserDetailsQueryHandler(IIdentityService identityService)
+        {
+            _identityService = identityService;
+        }
+        public async Task<UserDetailsResponseDTO> Handle(GetUserDetailsQuery request, CancellationToken cancellationToken)
+        {
+            var (userId, fullName, userName, email, roles) = await _identityService.GetUserDetailsAsync(request.UserId);
+            return new UserDetailsResponseDTO() { Id = userId, FullName = fullName, UserName = userName, Email = email, Roles = roles };
+        }
+    }
+}
