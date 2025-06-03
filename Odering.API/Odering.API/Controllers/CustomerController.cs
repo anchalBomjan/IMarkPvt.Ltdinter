35d91bf5e64a6a42450edc831f37f381c58d0adf
anchalBomjan <anchal.percoidit@gmail.com>
2025-06-03 13:57:06 +0545

    adding all Get methods for Customer in CustomerController

---
+++ Odering.API/Odering.API/Controllers/CustomerController.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Ordering.Application.Queries.Customers;
+using Ordering.Core.Entities;
+
+namespace Ordering.API.Controllers
+{
+
+    [EnableCors("CorsPolicy")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public CustomerController(IMediator mediator)
+        {
+            _mediator=mediator;
+        }
+        [HttpGet("GetAll")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<List<Customer>> Get()
+        {
+
+            return await _mediator.Send(new GetAllCustomerQuery());
+        }
+
+    }
+}
