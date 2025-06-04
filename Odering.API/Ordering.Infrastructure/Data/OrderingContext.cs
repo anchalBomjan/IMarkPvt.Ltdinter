e8e65c9df688866f88e5c1b70edef86d4f6f2707
anchalBomjan <anchal.percoidit@gmail.com>
2025-06-04 11:39:42 +0545

    modify orderingDbContext for IdentityService

--- Odering.API/Ordering.Infrastructure/Data/OrderingContext.cs
+++ Odering.API/Ordering.Infrastructure/Data/OrderingContext.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Ordering.Core.Entities;
+using Ordering.Infrastructure.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,13 +11,22 @@ using System.Threading.Tasks;
 
 namespace Ordering.Infrastructure.Data
 {
-    public  class OrderingContext:DbContext
+    public  class OrderingContext:IdentityDbContext<ApplicationUser,IdentityRole,string>
     {
+
         public OrderingContext(DbContextOptions<OrderingContext>options):base(options)
         {
             
         }
 
         public DbSet<Customer> Customers { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+
+
+            base.OnModelCreating(builder);
+        }
     }
 }
