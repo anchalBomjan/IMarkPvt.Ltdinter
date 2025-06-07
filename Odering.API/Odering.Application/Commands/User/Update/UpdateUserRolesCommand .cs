4da6feb6fa4c5125cb80e2d3e40f7d27ba2d686e
anchalBomjan <anchal.percoidit@gmail.com>
2025-06-07 21:52:51 +0545

    some changes

--- Odering.API/Odering.Application/Commands/User/Update/UpdateUserRolesCommand .cs
+++ Odering.API/Odering.Application/Commands/User/Update/UpdateUserRolesCommand .cs
@@ -1,4 +1,4 @@
-﻿using MediatR;
+﻿ using MediatR;
 using Ordering.Application.Common.Interface;
 using System;
 using System.Collections.Generic;
--- Odering.API/Ordering.Infrastructure/obj/Debug/net8.0/Ordering.Infrastructure.AssemblyInfo.cs
+++ Odering.API/Ordering.Infrastructure/obj/Debug/net8.0/Ordering.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ordering.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fc91a412a590767d55b9801038cebbe97f55e3b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c89609d501b3d455182bc65e42674a0172fa379e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ordering.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ordering.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
