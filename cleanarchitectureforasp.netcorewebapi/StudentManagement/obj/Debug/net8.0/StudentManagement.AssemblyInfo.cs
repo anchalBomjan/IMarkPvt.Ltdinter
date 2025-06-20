bed5f346dc3abca32752619bdf1d3e2ae0bd4ffd
anchalBomjan <anchal.percoidit@gmail.com>
2025-06-20 23:08:40 +0545

    adding clientapp for fontend

--- cleanarchitectureforasp.netcorewebapi/StudentManagement/obj/Debug/net8.0/StudentManagement.AssemblyInfo.cs
+++ cleanarchitectureforasp.netcorewebapi/StudentManagement/obj/Debug/net8.0/StudentManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b82a6e61b84d6af0177e754185be90110b6f005")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6afb70aa7871e179b8a8b3b34d21da7a32d002d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
