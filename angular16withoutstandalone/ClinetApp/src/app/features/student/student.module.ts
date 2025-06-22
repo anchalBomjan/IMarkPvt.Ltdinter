59b1117a2009b5864e20480409eba5de792549a2
anchalBomjan <anchal.percoidit@gmail.com>
2025-06-22 17:22:08 +0545

    somechanges

--- angular16withoutstandalone/ClinetApp/src/app/features/student/student.module.ts
+++ angular16withoutstandalone/ClinetApp/src/app/features/student/student.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 import { StudentRoutingModule } from './student-routing.module';
 import { StudentCreateComponent } from './components/student-create/student-create.component';
@@ -7,10 +6,13 @@ import { StudentEditComponent } from './components/student-edit/student-edit.com
 import { StudentListComponent } from './components/student-list/student-list.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import {  RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 
 @NgModule({
   declarations: [
+
+    //  imports modules are used by this components so declartion here
     StudentCreateComponent,
     StudentEditComponent,
     StudentListComponent
