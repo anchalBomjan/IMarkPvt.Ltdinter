7c8509de7ae06bc6fb8d7a00f5455a29c114dea1
anchalBomjan <anchal.percoidit@gmail.com>
2025-06-23 17:26:23 +0545

    someChanges in Create.Component

--- angular16withoutstandalone/ClinetApp/src/app/features/student/components/student-create/student-create.component.ts
+++ angular16withoutstandalone/ClinetApp/src/app/features/student/components/student-create/student-create.component.ts
@@ -1,7 +1,30 @@
+// import { Component } from '@angular/core';
+// import { Router } from '@angular/router';
+// import { Student } from 'src/app/core/models/student';
+// import { StudentService } from 'src/app/core/services/student.service';
+
+// @Component({
+//   selector: 'app-student-create',
+//   templateUrl: './student-create.component.html',
+//   styleUrls: ['./student-create.component.scss']
+// })
+// export class StudentCreateComponent {
+//   student: Student = { id: 0, name: '', email: '' };
+
+//   constructor(private studentService: StudentService, private router: Router) {}
+
+//   save(): void {
+//     this.studentService.create(this.student).subscribe(() => {
+//       this.router.navigate(['/students']);
+//     });
+//   }
+// }
+
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { Student } from 'src/app/core/models/student';
 import { StudentService } from 'src/app/core/services/student.service';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-student-create',
@@ -10,11 +33,30 @@ import { StudentService } from 'src/app/core/services/student.service';
 })
 export class StudentCreateComponent {
   student: Student = { id: 0, name: '', email: '' };
+  showDialog = false;
 
-  constructor(private studentService: StudentService, private router: Router) {}
+  constructor(
+    private studentService: StudentService,
+    private router: Router,
+    private messageService: MessageService
+  ) {}
+
+  openDialog() {
+    this.showDialog = true;
+  }
+
+  closeDialog() {
+    this.showDialog = false;
+  }
 
   save(): void {
     this.studentService.create(this.student).subscribe(() => {
+      this.messageService.add({
+        severity: 'success',
+        summary: 'Success',
+        detail: 'Student created successfully!'
+      });
+      this.closeDialog();
       this.router.navigate(['/students']);
     });
   }
