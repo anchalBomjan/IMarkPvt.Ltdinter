5b5080b68ef65ace1cd4e701e6c1dd32b6c8544d
anchalBomjan <anchal.percoidit@gmail.com>
2025-06-23 22:34:47 +0545

    Issuse fixed for toster

--- angular16withoutstandalone/ClinetApp/src/app/features/student/components/student-create/student-create.component.ts
+++ angular16withoutstandalone/ClinetApp/src/app/features/student/components/student-create/student-create.component.ts
@@ -1,25 +1,3 @@
-// import { Component } from '@angular/core';
-// import { Router } from '@angular/router';
-// import { Student } from 'src/app/core/models/student';
-// import { StudentService } from 'src/app/core/services/student.service';
-
-// @Component({
-//   selector: 'app-student-create',
-//   templateUrl: './student-create.component.html',
-//   styleUrls: ['./student-create.component.scss']
-// })
-// export class StudentCreateComponent {
-//   student: Student = { id: 0, name: '', email: '' };
-
-//   constructor(private studentService: StudentService, private router: Router) {}
-
-//   save(): void {
-//     this.studentService.create(this.student).subscribe(() => {
-//       this.router.navigate(['/students']);
-//     });
-//   }
-// }
-
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { Student } from 'src/app/core/models/student';
@@ -54,10 +32,16 @@ export class StudentCreateComponent {
       this.messageService.add({
         severity: 'success',
         summary: 'Success',
-        detail: 'Student created successfully!'
+        detail: 'Student created successfully!',
+        life: 2000
       });
+
       this.closeDialog();
-      this.router.navigate(['/students']);
+
+      // Delay navigation to let toast show
+      setTimeout(() => {
+        this.router.navigate(['/students']);
+      }, 2000);
     });
   }
 }
