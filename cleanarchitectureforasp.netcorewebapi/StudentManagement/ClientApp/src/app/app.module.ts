c69f67c02861675963a53fc5f0a58440624288f5
anchalBomjan <anchal.percoidit@gmail.com>
2025-06-24 15:45:17 +0545

    someChanges

--- cleanarchitectureforasp.netcorewebapi/StudentManagement/ClientApp/src/app/app.module.ts
+++ cleanarchitectureforasp.netcorewebapi/StudentManagement/ClientApp/src/app/app.module.ts
@@ -2,17 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './shared/components/navbar/navbar.component';
 import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from './shared/shared.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+
+
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule,   // this helps to run the angular Application
+    AppRoutingModule,
+    SharedModule,  // bezc app.component uses navbar
+    BrowserAnimationsModule,  // enables animations in our  Anular Application
+    HttpClientModule  // enables communication with backedend  services using HTTP Protocols
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- cleanarchitectureforasp.netcorewebapi/StudentManagement/ClientApp/src/app/features/student/student.module.ts
+++ cleanarchitectureforasp.netcorewebapi/StudentManagement/ClientApp/src/app/features/student/student.module.ts
@@ -5,6 +5,8 @@ import { StudentRoutingModule } from './student-routing.module';
 import { StudentListComponent } from './components/student-list/student-list.component';
 import { StudentEditComponent } from './components/student-edit/student-edit.component';
 import { StudentCreateComponent } from './components/student-create/student-create.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
@@ -15,7 +17,9 @@ import { StudentCreateComponent } from './components/student-create/student-crea
   ],
   imports: [
     CommonModule,
-    StudentRoutingModule
+    StudentRoutingModule,
+    SharedModule,
+    RouterModule
   ]
 })
 export class StudentModule { }
