4951c7b38f106d04e50ce8d666722c7e3d38165a
anchalBomjan <anchal.percoidit@gmail.com>
2025-06-24 17:13:56 +0545

    arranging studentroute and approute

--- cleanarchitectureforasp.netcorewebapi/StudentManagement/ClientApp/src/app/app-routing.module.ts
+++ cleanarchitectureforasp.netcorewebapi/StudentManagement/ClientApp/src/app/app-routing.module.ts
@@ -1,12 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes:Routes=[
+  {
+    path:'students',
+    loadChildren:()=> import('./features/student/student.module').then(m=> m.StudentModule)
+  },
+  {
+    path:'',redirectTo:'students',pathMatch:'full'
+  }
+
+
+];
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    RouterModule.forRoot(routes)]
+,
+exports:[RouterModule]
+
 })
 export class AppRoutingModule { }
--- cleanarchitectureforasp.netcorewebapi/StudentManagement/ClientApp/src/app/features/student/student-routing.module.ts
+++ cleanarchitectureforasp.netcorewebapi/StudentManagement/ClientApp/src/app/features/student/student-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StudentListComponent } from './components/student-list/student-list.component';
+import { StudentCreateComponent } from './components/student-create/student-create.component';
+import { StudentEditComponent } from './components/student-edit/student-edit.component';
 
-const routes: Routes = [];
+const routes:Routes=[
+{path:'',component:StudentListComponent},
+{path:'students',component:StudentListComponent},
+{path:'students/create',component:StudentCreateComponent},
+{path:'students/edit/:id',component:StudentEditComponent}
 
+];
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
