21820cc7f3ac62913efe9eba949025d4b9112bb3
anchalBomjan <anchal.percoidit@gmail.com>
2025-06-26 18:15:35 +0545

    implemetation for edit

--- cleanarchitectureforasp.netcorewebapi/StudentManagement/ClientApp/src/app/features/student/components/student-create/student-create.component.ts
+++ cleanarchitectureforasp.netcorewebapi/StudentManagement/ClientApp/src/app/features/student/components/student-create/student-create.component.ts
@@ -12,10 +12,35 @@ import { NavigationEnd, Router } from '@angular/router';
 })
 export class StudentCreateComponent {
   @Input() showCreateDialog: boolean = false;
+
+
+
+  private _student: IStudent = { id: 0, name: '', email: '', age: 0 };
+  @Input()
+  set student(value: IStudent) {
+    this._student = { ...value };
+    this.newStudent = { ...this._student }; // update internal model immediately
+  }
+  get student(): IStudent {
+    return this._student;
+  }
+
+  private _mode: 'create' | 'edit' = 'create';
+  @Input()
+  set mode(value: 'create' | 'edit') {
+    this._mode = value;
+  }
+  get mode(): 'create' | 'edit' {
+    return this._mode;
+  }
   @Output() closeDialog = new EventEmitter<void>(); // Emits when the dialog needs to close
   @Output() studentSaved = new EventEmitter<void>();
 
-  newStudent: IStudent = { id: 0, name: '', email: '', age: 0 };
+
+ 
+
+
+  newStudent: IStudent = { id: 0, name: '', email: '', age: 0 }; //  this   id for create/student
 
   constructor( private studentService: StudentService,private messageService: MessageService ,private router:Router) 
   {
@@ -32,7 +57,7 @@ export class StudentCreateComponent {
     })
   }
 
-  // this  will provided values to open p diaglog box of component.html 
+  // this  will provide values to open p-diaglog box component.html 
   checkRoute(){
      // Listen to route changes
      this.router.events.subscribe(event => {
@@ -47,39 +72,63 @@ export class StudentCreateComponent {
 
   saveStudent() {
     console.log('Saving Student:', this.newStudent);
-    this.studentService.addStudent(this.newStudent).subscribe({
-      next: () => {
-        this.messageService.add({
-          severity: 'success',
-          summary: 'Success',
-          detail: 'Student added successfully'
-        });
-        this.studentSaved.emit(); // Notify parent to reload students
-       this.closeDialog.emit();  // Close the dialog
-       this.resetForm();
 
-        /// this below for create/student
-        this.showCreateDialog=false;
-       // this.router.navigate(['/students'])
-
-        setTimeout(() => {
-          this.router.navigate(['/students']);
-        }, 2000);
-        
-      },
-      error: (error) => {
-        console.error('Error saving student', error);
-        this.messageService.add({
-          severity: 'error',
-          summary: 'Error',
-          detail: 'Failed to save student'
+    if (this.mode=='edit')
+    {
+      if(this.newStudent.id!==undefined){
+        this.studentService.updateStudent(this.newStudent.id, this.student).subscribe({
+          next: () => {
+            this.messageService.add({ severity: 'success', summary: 'Updated', detail: 'Student updated successfully' });
+            this.studentSaved.emit();
+            this.closeDialog.emit();
+          },
+          error: () => {
+            this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Update failed' });
+          }
+  
         });
-        setTimeout(() => {
-          this.router.navigate(['/students']);
-        }, 2000);
+
       }
-      
-    });
+    
+
+    }
+    else{
+      this.studentService.addStudent(this.newStudent).subscribe({
+        next: () => {
+          this.messageService.add({
+            severity: 'success',
+            summary: 'Success',
+            detail: 'Student added successfully'
+          });
+          this.studentSaved.emit(); // Notify parent to reload students
+         this.closeDialog.emit();  // Close the dialog
+         this.resetForm();
+  
+          /// this below for create/student
+          this.showCreateDialog=false;
+         // this.router.navigate(['/students'])
+  
+          setTimeout(() => {
+            this.router.navigate(['/students']);
+          }, 2000);
+          
+        },
+        error: (error) => {
+          console.error('Error saving student', error);
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Error',
+            detail: 'Failed to save student'
+          });
+          setTimeout(() => {
+            this.router.navigate(['/students']);
+          }, 2000);
+        }
+        
+      });
+
+    }
+  
   }
 
 
--- cleanarchitectureforasp.netcorewebapi/StudentManagement/ClientApp/src/app/features/student/components/student-list/student-list.component.ts
+++ cleanarchitectureforasp.netcorewebapi/StudentManagement/ClientApp/src/app/features/student/components/student-list/student-list.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { IStudent } from 'src/app/core/models/student';
 import { MessageService } from 'primeng/api';
-import { Router } from '@angular/router';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { StudentService } from 'src/app/core/services/Student.Service';
 
 @Component({
@@ -12,16 +12,36 @@ import { StudentService } from 'src/app/core/services/Student.Service';
 })
 export class StudentListComponent {
   students: IStudent[] = [];
-
   showDeleteDialog = false;
+  showCreateDialog = false;
   selectedStudentId: number | null = null;
 
-  showCreateDialog = false;
+
+  mode:'create'|'edit'='create';
   newStudent: IStudent = {id:0, name: '', email: '', age: 0 };
 
-  constructor( private studentService: StudentService, private messageService: MessageService,private router: Router,)
+
+   constructor( private studentService: StudentService, private messageService: MessageService,private router: Router,private route:ActivatedRoute)
    {
     this.loadStudents();
+    
+ 
+
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        const url = event.urlAfterRedirects;
+  
+        const id = this.route.snapshot.paramMap.get('id');
+  
+        if (url.includes('/students/edit') && id) {
+          this.openEditDialog(+id);
+        }
+  
+        if (url.includes('/students/create')) {
+          this.openCreateDialog();
+        }
+      }
+    });
    }
 
   loadStudents(): void {
@@ -32,6 +52,20 @@ export class StudentListComponent {
     });
   }
 
+  openEditDialog(id:number){
+    this.mode='edit';
+    this.studentService.getStudentById(id).subscribe(student=>{
+      console.log("edit details:", student);
+      //this.newStudent={...student};
+
+      this.newStudent.id=student.id;
+      this.newStudent.name=student.name;
+      this.newStudent.email=student.email;
+      this.newStudent.age=student.age;
+
+      this.showCreateDialog =true;
+    })
+  }
   openDeleteDialog(id: number) {
     this.selectedStudentId = id;
     this.showDeleteDialog = true;
@@ -63,33 +97,44 @@ export class StudentListComponent {
 
   saveStudent() {
     console.log('Saving Student:', this.newStudent);// Log the student data veing sent
-    this.studentService.addStudent(this.newStudent).subscribe({
+    if(this.mode=='edit'){
+
+      
+
+    } else{
+      this.studentService.addStudent(this.newStudent).subscribe({
    
-      next:(response)=>{
-        console.log('Student saved successfully:',response);
+        next:(response)=>{
+          console.log('Student saved successfully:',response);
+          this.messageService.add({
+            severity:'success',
+            summary:'success',
+            detail:'Student added successfully'
+  
+          });
+  
+          this.showCreateDialog=false;
+          this.loadStudents();
+        },
+  
+        error:(error)=>{
+        console.log('Error saving student',error);
         this.messageService.add({
-          severity:'success',
-          summary:'success',
-          detail:'Student added successfully'
+          severity:'error',
+          summary:'eror',
+          detail:' fail to load'
+        })
+        }
+      });
 
-        });
 
-        this.showCreateDialog=false;
-        this.loadStudents();
-      },
 
-      error:(error)=>{
-      console.log('Error saving student',error);
-      this.messageService.add({
-        severity:'error',
-        summary:'eror',
-        detail:' fail to load'
-      })
-      }
-    });
+    }
+  
   }
   editStudent(id: number) {
     this.router.navigate(['/students/edit', id]);
   }
+
 }
 
